225d06a9a19dc2857d98a571a5dc7410b9a7b942
ptotet <122583128+Prouter1@users.noreply.github.com>
2024-02-10 01:05:08 +0700

    Update getFrameHtml.ts

--- app/lib/getFrameHtml.ts
+++ app/lib/getFrameHtml.ts
@@ -4,9 +4,9 @@ export function getFrameHtml(frameMetadata: FrameMetadataType) {
   const html = getFrameHtmlResponse(frameMetadata);
 
   const extraTags = [
-    '<meta property="og:title" content="Farcaster: Horse">',
-    '<meta property="og:description" content="Farcaster Protocol Release">',
-    '<meta property="og:image" content="https://mint.farcaster.xyz/api/images/start">',
+    '<meta property="og:title" content="Farcaster: Pioneers">',
+    '<meta property="og:description" content="Pioneers">',
+    '<meta property="og:image" content="https://zora.co/collect/base:0xb71124ba2a14ab279223492cd9065c6e6aaabfa1/2">',
     '<meta property="fc:frame:image:aspect_ratio" content="1:1" />',
   ];
   // hack: remove close tags, add aspect ratio and required OG tags
