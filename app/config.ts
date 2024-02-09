2a42a5ccbb8a83f27cca5f4fea083b94768f4bf6
ptotet <122583128+Prouter1@users.noreply.github.com>
2024-02-10 01:07:00 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
-export const NEXT_PUBLIC_URL = 'https://mint.farcaster.xyz';
-export const ALLOWED_ORIGIN = 'mint.farcaster.xyz';
-export const ZORA_COLLECTION_ADDRESS = '0xf5a3b6dee033ae5025e4332695931cadeb7f4d2b';
-export const ZORA_TOKEN_ID = '1';
+export const NEXT_PUBLIC_URL = 'https://zora.co/collect/base:0xb71124ba2a14ab279223492cd9065c6e6aaabfa1/2';
+export const ALLOWED_ORIGIN = 'https://zora.co/collect/base:0xb71124ba2a14ab279223492cd9065c6e6aaabfa1/2';
+export const ZORA_COLLECTION_ADDRESS = '0xb71124ba2a14ab279223492cd9065c6e6aaabfa1';
+export const ZORA_TOKEN_ID = '2';
 export const CARD_DIMENSIONS = {
   width: 800,
   height: 800,
